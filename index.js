5e36561c09efb5ca7a631ce7110ba7ed4c2f2568
PlayBy <bigbrosebas@gmail.com>
2018-05-14 20:55:08 -0400

    gave the bracket content, this will fix it...

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ class Client extends EventEmitter {
                         this.rooms.push(JSON.parse(data).details.room);
 
                     }
-                } catch () {
+                } catch (err) {
                     this.emit('message', data);
                 }
             });
